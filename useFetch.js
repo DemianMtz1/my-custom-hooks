df4bc502ed60e8d8d20accdced6e4a361c5bfa8a
Eric Demian Martinez Valdez <ericdemian.martinez@gmail.com>
2020-10-27 15:34:05 -0600

    First commit

---
+++ useFetch.js
@@ -0,0 +1,42 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useFetch = (url) => {
+    const isMounted = useRef(true);
+    const [state, setState] = useState({ data: null, loading: true, error: null });
+
+    useEffect(() => {
+        // se limpiara el setState previniendo el error de que no se ha montado una operacion
+        return () => {
+            isMounted.current = false;
+        }
+    }, [])
+
+    useEffect(() => {
+
+        setState({ data: null, loading: true, error: null })
+
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+
+
+
+                
+                    if (isMounted.current) {
+                        setState({
+                            loading: false,
+                            error: null,
+                            data
+                        });
+                    } else {
+                        console.log('setState no se monto');
+                    }
+                
+
+
+            });
+
+    }, [url]);
+
+    return state;
+}
